d9da8a13dd30ec766682ce619fa04d46f74f7496
Dhanush S Gowda <dhanushsgowda277@gmail.com>
2024-12-12 14:29:16 +0530

    deploy using gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/masala-marvel-restaurant-website/', // Add your repository name here
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
